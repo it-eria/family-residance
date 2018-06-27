090acd122c618a6fd25253f66ec5de8f583dbde5
Mykola Ferents <ferents911@gmail.com>
2018-06-27 18:52:29 +0300

    27/06/2018

--- src/js/script.js
+++ src/js/script.js
@@ -4,47 +4,18 @@ $(function() {
     $(this).toggleClass('clicked');
     $('.header__nav-section').fadeToggle(300);
   });
-  $('.custom-select').niceSelect();
+  $('.custom-select, select').niceSelect();
+
+  $('input[name="taxi-service"]:checked').parent().addClass('selected');
+
+  $('input[name="taxi-service"]').on('change', function() {
+    $('input[name="taxi-service"]').parent().removeClass('selected');
+    $(this).parent().addClass('selected');
+  });
+
+  $('body').removeAttr('class');
 });
-
-function iteriaWatermark() {
-  console.log("            hMMMMMMMy       :MMMMMMMM`            ");
-  console.log("            hMM                   yMM`            ");
-  console.log("            hMM      /oooooo`     yMM`            ");
-  console.log("            hMM      sdddddd`     yMM`            ");
-  console.log("            hMM      sdddddd`     yMM`            ");
-  console.log("            hMM      /oooooo`     yMM`            ");
-  console.log("            hMM                   yMM`            ");
-  console.log("            hMMMMMMMy       :MMMMMMMM`            ");
-  console.log("                                                  ");
-  console.log(".` .::::::::` -oooooo.  ooo++:    :.      /.      ");
-  console.log("/-     :-     /+        m   `/m`  s:     /hd`     ");
-  console.log("/-     :-     /+        m    `m-  s:    -d`:h     "); 
-  console.log("/-     :-     /hoooo+   N+++yo-   s:   `m:``ys    ");
-  console.log("/-     :-     /+        m   oy`   s:   do++++h/   ");
-  console.log("/-     :-     /o        m    :d.  s:  yo     `m-  ");
-  console.log(".`     ``     .//////\`  :     .:  -` `/       .: ");
-}
-
-// iteriaWatermark();
 
 AOS.init();
 
 
-// Google Maps
-function initMap() {
-  var markerPos = {lat: 49.840594, lng: 24.023675};
-  var centerPos = {lat: markerPos.lat, lng: markerPos.lng - 0.001};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: centerPos,
-    disableDefaultUI: true
-  });
-  
-  var marker = new google.maps.Marker({
-    position: markerPos,
-    map: map,
-    icon: 'assets/img/mapID.png',
-    title: 'Family residence'
-  });
-}
